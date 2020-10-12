dac6051b3f697a3fa6d3706a3c3c8b81c6f53bc6
卢松林 <2445859460@qq.com>
2020-10-12 16:57:01 +0800

    update Hibernate

---
+++ src/test/java/edu/xcu/Hibernate/Mapper/EmployeeMapperTest.java
@@ -0,0 +1,60 @@
+package edu.xcu.Hibernate.Mapper;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @project: JavaLearn
+ * @description:
+ * @author: dell
+ * @date: 2020/10/12 - 16:43
+ * @version: 1.0
+ * @website:
+ */
+public class EmployeeMapperTest {
+    private EmployeeMapper employeeMapper;
+    private Integer empID1;
+    private Integer empID2;
+    private Integer empID3;
+    @Before
+    public void init(){
+        try{
+            SessionFactory factory = new Configuration().configure().buildSessionFactory();
+            employeeMapper = new EmployeeMapper(factory);
+        }catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    @Test
+    public void addEmployee() {
+
+        /* Add few employee records in database */
+        empID1 = employeeMapper.addEmployee("Zara", "Ali", 1000);
+        empID2 = employeeMapper.addEmployee("Daisy", "Das", 5000);
+        empID3 = employeeMapper.addEmployee("John", "Paul", 10000);
+    }
+
+    @Test
+    public void listEmployees() {
+        /* List down all the employees */
+        employeeMapper.listEmployees();
+    }
+
+    @Test
+    public void updateEmployee() {
+        /* Update employee's records */
+        employeeMapper.updateEmployee(4, 5000);
+    }
+
+    @Test
+    public void deleteEmployee() {
+        /* Delete an employee from the database */
+        employeeMapper.deleteEmployee(4);
+    }
+}
